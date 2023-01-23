fc6f7d0b792160c9c26c81ddf2a87bf0529c0a53
Scott Clingan <sclingan1@gmail.com>
2023-01-23 15:19:45 -0600

    Setting up project

--- src/App.js
+++ src/App.js
@@ -1,26 +1,36 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
   <main>
-     Order Summary
+    <div className="div__card">
+      <header>
+        <img src='./images/illustration-hero.svg' alt=""></img>
+      </header>
+      <div className="div__card_info">
+        <h1>Order Summary</h1>
+        <p>
+          You can now listen to millions of songs, audiobooks, and podcasts on any 
+          device anywhere you like!
+        </p>
+        <div className="div__card_plan">
+         Annual Plan
+         $59.99/year
 
-    You can now listen to millions of songs, audiobooks, and podcasts on any 
-    device anywhere you like!
-
-    Annual Plan
-    $59.99/year
-
-    Change
-
-    Proceed to Payment
-    Cancel Order
-
-    <div class="attribution">
-      Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">Frontend Mentor</a>. 
-      Coded by <a href="#">Scott Clingan</a>.
+         Change
+      </div>
+        Proceed to Payment
+        Cancel Order
+      </div>
+    
     </div>
+
+    <footer>
+      <div class="attribution">
+        Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">Frontend Mentor</a>. 
+        Coded by <a href="#">Scott Clingan</a>.
+      </div>
+    </footer>
   </main>
   );
 }
